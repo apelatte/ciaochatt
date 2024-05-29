c44a766b3244ad7232104706a97dc9f4463344d8
apelatte <palapinto10@gmail.com>
2024-05-29 17:33:12 +0200

    feat(chat service impl): add chat service implementation

---
+++ ciao/src/main/java/com/chat/ciao/services/impl/ChatServiceImpl.java
@@ -0,0 +1,42 @@
+package com.chat.ciao.services.impl;
+
+import com.chat.ciao.dao.iChatDao;
+import com.chat.ciao.models.Chat;
+import com.chat.ciao.models.User;
+import com.chat.ciao.services.iChatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ChatServiceImpl implements iChatService {
+
+  @Autowired
+  private iChatDao chatDao;
+
+  @Override
+  public List<Chat> findAll() {
+    return this.chatDao.findAll();
+  }
+
+  @Override
+  public Chat findById(long id) {
+    return this.chatDao.findById(id).orElse(null);
+  }
+
+  @Override
+  public List<Chat> findChatsByUser(User user) {
+    return this.chatDao.findByParticipantsLike(user).orElse(null);
+  }
+
+  @Override
+  public Chat save(Chat chat) {
+    return this.chatDao.save(chat);
+  }
+
+  @Override
+  public void delete(Chat chat) {
+    this.chatDao.delete(chat);
+  }
+}
